89d4c86744e90ab8deb1bd8ae87b5276cbef3afe
usuario <user@gmail.com>
2024-01-26 12:33:18 +0100

    primer envio

---
+++ index.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Inicio</h2>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Welcome</h2>
+<p>Introduce tu consulta</p>
+<form>
+    <input type="text" id="consulta" name="consulta"
+    <input type="submit" value="Consultar">
+</form>
+<a href="#">Cerrar sesion - logout</a>
+</body>
+</html>
